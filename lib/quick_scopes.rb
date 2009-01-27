5e877f186e565150400207741038170d70087d51
Andrew Timberlake <andrew@andrewtimberlake.com>
2009-01-27 10:32:22 +0200

    Fixing missing brace

--- lib/quick_scopes.rb
+++ lib/quick_scopes.rb
@@ -2,5 +2,5 @@ class ActiveRecord::Base
   named_scope :order, lambda { |order| {:order => order} }
   named_scope :sort, lambda { |order| {:order => order} }
   named_scope :limit, lambda { |limit| {:limit => limit} }
-  named_scope :include, lambda { |include| {:include => include }
+  named_scope :include, lambda { |include| {:include => include } }
 end
